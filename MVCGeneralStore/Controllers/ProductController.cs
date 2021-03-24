6addfcdc029a9f76b4dd0ff9d453f409ac5e526d
bmcdougalle <bmcdougalle@gmail.com>
2021-03-23 21:15:48 -0400

    Added productController methods and Product create and delete views

--- MVCGeneralStore/Controllers/ProductController.cs
+++ MVCGeneralStore/Controllers/ProductController.cs
@@ -15,5 +15,53 @@ namespace MVCGeneralStore.Controllers
         {
             return View(_db.Products.ToList());
         }
+
+        //Get: Product
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Post: Create
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(product);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        //Get: Delete
+        // Product/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            Product product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+
+        }
+
+        //Post: Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Product product = _db.Products.Find(id);
+            _db.Products.Remove(product);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
