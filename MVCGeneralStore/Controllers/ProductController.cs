ddee1dd8f2cc976a5adbc0b31ca0cf72abf083dc
bmcdougalle <bmcdougalle@gmail.com>
2021-03-23 21:29:44 -0400

    Added Edit View and controller method

--- MVCGeneralStore/Controllers/ProductController.cs
+++ MVCGeneralStore/Controllers/ProductController.cs
@@ -63,5 +63,37 @@ namespace MVCGeneralStore.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        //Get : Edit
+        //Product/Edit/{Id}
+        public ActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            Product product = _db.Products.Find(id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(product);
+        }
+
+        //Post : Edit// Product/Edit/{id}
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
     }
 }
