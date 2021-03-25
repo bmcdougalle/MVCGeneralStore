c61dff02520850c6e7ad6764734d5fd70874f5ba
bmcdougalle <bmcdougalle@gmail.com>
2021-03-25 19:17:30 -0400

    Added create and Delete methods in controller and in views

--- MVCGeneralStore/Controllers/CustomerController.cs
+++ MVCGeneralStore/Controllers/CustomerController.cs
@@ -16,9 +16,50 @@ namespace MVCGeneralStore.Controllers
             return View();
         }
 
+        //Get : customer
         public ActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Customers.Add(customer);
+                _db.SaveChanges();
+
+                return RedirectToAction("index");
+            }
+
+            return View(customer);
+        }
+
+        //Get : Delete
+        public ActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            Customer customer = _db.Customers.Find(id);
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Customer customer = _db.Customers.Find(id);
+            _db.Customers.Remove(customer);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
