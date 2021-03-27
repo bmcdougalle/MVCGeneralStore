4f0ebbb7ccf8367d7c957ff4b02a19309ccf2cf4
bmcdougalle <bmcdougalle@gmail.com>
2021-03-27 11:52:50 -0400

    Added transaction action Methods and views

--- MVCGeneralStore/Controllers/TransactionController.cs
+++ MVCGeneralStore/Controllers/TransactionController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCGeneralStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,118 @@ namespace MVCGeneralStore.Controllers
 {
     public class TransactionController : Controller
     {
+        private ApplicationDbContext _db = new ApplicationDbContext();
         // GET: Transaction
         public ActionResult Index()
         {
+            return View(_db.Transactions.ToArray());
+        }
+
+        public ActionResult Create()
+        {
+            ViewData["Products"] = _db.Products.Select(p => new SelectListItem
+            {
+                Text = p.Name,
+                Value = p.ProductId.ToString()
+            });
+
+            ViewData["Customers"] = _db.Customers.Select(p => new SelectListItem
+            {
+                Text = p.FirstName + ' ' ,
+                Value = p.CustomerID.ToString()
+            });
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Transaction model)
+        {
+            //model.DateOfTransaction = DateTime.Now;
+
+            var createdObj = _db.Transactions.Add(model);
+            if(_db.SaveChanges() == 1)
+            {
+                return Redirect("/Transaction/" +
+                    createdObj.TransactionId);
+            }
+            return View(model);
+        }
+
+        public ActionResult Details(int transactionId)
+        {
+            var transaction = _db.Transactions.Find(transactionId);
+            return View(transaction);
+        }
+
+        public ActionResult Edit(int transactionId)
+        {
+            var transaction = _db.Transactions.Find(transactionId);
+            if(transaction == null)
+            {
+                return Redirect("Index");
+            }
+
+            ViewData["Products"] = _db.Products.Select(p => new SelectListItem
+            {
+                Text = p.Name,
+                Value = p.ProductId.ToString()
+            });
+
+            ViewData["Customers"] = _db.Customers.Select(p => new SelectListItem
+            {
+                Text = p.FirstName + ' ',
+                Value = p.CustomerID.ToString()
+            });
+            return View(transaction);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Transaction model)
+        {
+            var entity = _db.Transactions.Find(model.TransactionId);
+            entity.CustomerId = model.CustomerId;
+            entity.ProductId = model.ProductId;
+            if(_db.SaveChanges() == 1)
+            {
+                return Redirect("Index");
+            }
+
+            ViewData["Products"] = _db.Products.Select(p => new SelectListItem
+            {
+                Text = p.Name,
+                Value = p.ProductId.ToString()
+            });
+
+            ViewData["Customers"] = _db.Customers.Select(p => new SelectListItem
+            {
+                Text = p.FirstName + ' ',
+                Value = p.CustomerID.ToString()
+            });
+
+            return View(model);
+        }
+
+        public ActionResult Delete(int transactionId)
+        {
+            var transaction = _db.Transactions.Find(transactionId);
+            if(transaction == null)
+            {
+                return Redirect("Index");
+            }
+            return View(transaction);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Transaction transaction)
+        {
+            var entity = _db.Transactions.Find(transaction.TransactionId);
+            _db.Transactions.Remove(entity);
+            if(_db.SaveChanges() == 1)
+            {
+                return Redirect("Index");
+            }
+
+            return View(transaction);
         }
     }
 }
